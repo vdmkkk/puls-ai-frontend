5fc3be73a0e791743d1a39c5c268541870b4807c
vdmkkk <soloviov.vad@gmail.com>
2025-01-27 10:55:14 +0300

    init app

---
+++ app/src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered style="background-color: #2d2d37">
+      <q-list>
+        <q-item-label style="color: white"> Тут будут разделы </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
